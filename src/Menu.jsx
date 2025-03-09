7d5ae540f5afb2b501cd8d80e704ec56f16c22e4
Charles7458 <c.charles8547@gmail.com>
2025-03-09 14:16:15 +0530

    updated nike.css

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -5,7 +5,7 @@ export default function () {
     return (
         <div style={{width:"100vw",overflowX:"hidden"}}>
 
-                <div style={{position:"relative",top:"0", padding:"10px 0px", marginBottom:"5px",width:'100%',backgroundColor:"green", color:"white", textOverflow:"e",fontSize:"clamp(9px,3vw,20px)"}}>
+                <div style={{position:"relative",top:"0", padding:"10px 0px", marginBottom:"5px",width:'100%',backgroundColor:"green", color:"white", textOverflow:"e",fontSize:"clamp(9px,3vw,16px)"}}>
                     <ul style={{listStyleType:"none", display:"flex", width:"90%", justifyContent:"space-between", alignItems:"center", textOverflow:"ellipsis"}}>
                         <Link to="/"><li style={{color:"white", width:"20%", textOverflow:"ellipsis", whiteSpace:"nowrap"}}title='Home'>Home</li></Link> |
                         <Link to="/amazony"><li style={{color:"white", width:"20%", textOverflow:"ellipsis", whiteSpace:"nowrap"}} title='Amazon(Not the real one)'>Amazon(Not the real one)</li></Link> |
