6d760c1c4c38a81cc23f74d4c6bbf0d3f6d5dc01
Charles7458 <c.charles8547@gmail.com>
2025-03-12 00:32:29 +0530

    updated Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,6 @@ export default function Home() {
             <h1 className="p-5 bg-primary text-white">
                 Thanks for viewing my project :)<br/>
                 Use the above links to navigate.<br/>
-                Only works well on Desktop for now.
             </h1>
         </div>
      );
